90b787fec16426c739b0dfeca94fbd244765f097
haryun <lharyun@github.com>
2025-10-23 22:29:13 +0900

    chargePoint_Sum_Value_over_MaxPoint and point_Under_MinPoint

--- src/test/java/io/hhplus/tdd/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceTest.java
@@ -20,10 +20,15 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class PointServiceTest {
+    //포인트 최대값
+    private static final long POINT_MAX = 10000L;
+    //포인트 최소값
+    private static final long POINT_MIN = 1L;
 
     private PointService pointService;
     private UserPointTable userPointTable;
     private PointHistoryTable pointHistoryTable;
+
 
     @BeforeEach
     void setUp() {
@@ -108,6 +113,39 @@ public class PointServiceTest {
     }
 
     @Test
+    @DisplayName("포인트충전 - 포인트 합산 값 최대 값 초과")
+    void chargePoint_Sum_Max_value_over() {
+        // Given
+        long userId = 1L;
+        long currentPoint = 90000L;
+        long chargeAmount = 20000L;  // 90000 + 20000 = 110000 > 100000
+        long currentTime = System.currentTimeMillis();
+
+        UserPoint currentUserPoint = new UserPoint(userId, currentPoint, currentTime);
+        when(userPointTable.selectById(userId)).thenReturn(currentUserPoint);
+
+        // When & Then
+        assertThatThrownBy(() -> pointService.chargePoint(userId, chargeAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("포인트 최대값을 초과했습니다.");  // "최대 포인트" 또는 "최대값" 포함
+    }
+
+    @Test
+    @DisplayName("포인트충전 - 포인트 값이 1보다 작은경우")
+    void chargePoint_Point_Under_MinPoint(){
+        //Given
+        long id =1L;
+        long amount = -1000L;
+
+        //when&then
+        assertThatThrownBy(() -> pointService.chargePoint(id, amount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("포인트 충전 최소금액은 1 이상이여야 합니다.");
+    }
+
+
+
+    @Test
     @DisplayName("포인트충전 예외케이스 - 음수 ID")
     void chargePoint_Minus_ID(){
         // Given
@@ -118,7 +156,6 @@ public class PointServiceTest {
         assertThatThrownBy(()-> pointService.chargePoint(invalidId,amount))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("유효하지 않은 사용자 ID 입니다.");
-
     }
 
     @Test
